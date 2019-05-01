9bb03e0a8abedee2e98b818c4accc8b1dbe976b3
boyska <piuttosto@logorroici.org>
2019-05-01 21:17:39 +0200

    some tests for rfc3164: GNU is failing!

--- format/rfc3164_test.go
+++ format/rfc3164_test.go
@@ -8,3 +8,34 @@ func (s *FormatSuite) TestRFC3164_SingleSplit(c *C) {
 	f := RFC3164{}
 	c.Assert(f.GetSplitFunc(), IsNil)
 }
+
+func (s *FormatSuite) TestRFC3164_CorrectParsingTypical(c *C) {
+	f := RFC3164{}
+
+	find := []string{
+		`<13>May  1 20:51:40 myhostname myprogram: ciao`,
+	}
+	parser := f.GetParser([]byte(find[0]))
+	err := parser.Parse()
+	c.Assert(err, IsNil)
+	c.Assert(parser.Dump()["content"], Equals, "ciao")
+	c.Assert(parser.Dump()["hostname"], Equals, "myhostname")
+	c.Assert(parser.Dump()["tag"], Equals, "myprogram")
+
+}
+
+func (s *FormatSuite) TestRFC3164_CorrectParsingGnu(c *C) {
+	// GNU implementation of syslog() has a variant: hostname is missing
+	f := RFC3164{}
+
+	find := []string{
+		`<13>May  1 20:51:40 myprogram: ciao`,
+	}
+	parser := f.GetParser([]byte(find[0]))
+	err := parser.Parse()
+	c.Assert(err, IsNil)
+	c.Assert(parser.Dump()["content"], Equals, "ciao")
+	// c.Assert(parser.Dump()["hostname"], Equals, "myhostname")
+	c.Assert(parser.Dump()["tag"], Equals, "myprogram")
+
+}
