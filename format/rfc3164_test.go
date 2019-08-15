42e2f02e9ccf46f8687d3b2e6074188e3bbf1fea
BoySka <boyska@users.noreply.github.com>
2019-08-15 22:19:58 +0200

    Fix GNU syslog (#63)
    * some tests for rfc3164: GNU is failing!
    
    * rfc3164 is now compatible with GNU syslog()
    
    * add a test case for PID in "normal" 3164 format

--- format/rfc3164_test.go
+++ format/rfc3164_test.go
@@ -8,3 +8,57 @@ func (s *FormatSuite) TestRFC3164_SingleSplit(c *C) {
 	f := RFC3164{}
 	c.Assert(f.GetSplitFunc(), IsNil)
 }
+
+func (s *FormatSuite) TestRFC3164_CorrectParsingTypical(c *C) {
+	f := RFC3164{}
+
+	find := `<13>May  1 20:51:40 myhostname myprogram: ciao`
+	parser := f.GetParser([]byte(find))
+	err := parser.Parse()
+	c.Assert(err, IsNil)
+	c.Assert(parser.Dump()["content"], Equals, "ciao")
+	c.Assert(parser.Dump()["hostname"], Equals, "myhostname")
+	c.Assert(parser.Dump()["tag"], Equals, "myprogram")
+
+}
+func (s *FormatSuite) TestRFC3164_CorrectParsingTypicalWithPID(c *C) {
+	f := RFC3164{}
+
+	find := `<13>May  1 20:51:40 myhostname myprogram[42]: ciao`
+	parser := f.GetParser([]byte(find))
+	err := parser.Parse()
+	c.Assert(err, IsNil)
+	c.Assert(parser.Dump()["content"], Equals, "ciao")
+	c.Assert(parser.Dump()["hostname"], Equals, "myhostname")
+	c.Assert(parser.Dump()["tag"], Equals, "myprogram")
+
+}
+
+func (s *FormatSuite) TestRFC3164_CorrectParsingGNU(c *C) {
+	// GNU implementation of syslog() has a variant: hostname is missing
+	f := RFC3164{}
+
+	find := `<13>May  1 20:51:40 myprogram: ciao`
+	parser := f.GetParser([]byte(find))
+	err := parser.Parse()
+	c.Assert(err, IsNil)
+	c.Assert(parser.Dump()["content"], Equals, "ciao")
+	// c.Assert(parser.Dump()["hostname"], Equals, "myhostname")
+	c.Assert(parser.Dump()["tag"], Equals, "myprogram")
+
+}
+
+func (s *FormatSuite) TestRFC3164_CorrectParsingJournald(c *C) {
+	// GNU implementation of syslog() has a variant: hostname is missing
+	// systemd uses it, and typically also passes PID
+	f := RFC3164{}
+
+	find := `<78>May  1 20:51:02 myprog[153]: blah`
+	parser := f.GetParser([]byte(find))
+	err := parser.Parse()
+	c.Assert(err, IsNil)
+	c.Assert(parser.Dump()["content"], Equals, "blah")
+	// c.Assert(parser.Dump()["hostname"], Equals, "myhostname")
+	c.Assert(parser.Dump()["tag"], Equals, "myprog")
+
+}
